bd979e69ec787882f30a39ac6929cfab9d932237
Davydoe <the-darksyde@hotmail.fr>
2016-12-07 19:13:19 +0100

    Ajout ennemi qui se deplace d'un point a vers b

---
+++ ProjectUnity/TrumpProject/Assets/Damage.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+    public Rigidbody2D rb2d;
+    public GameObject sound;
+
+	// Use this for initialization
+
+	void Start () {
+        
+	
+	}
+
+    public IEnumerator Knockback(float knockDur, float knockbackPwr, Vector3 knockbackDir)
+    {
+        float timer = 0;
+        while (knockDur > timer)
+        {
+            timer += Time.deltaTime;
+            rb2d.AddForce(new Vector3(knockbackDir.x * +100, knockbackDir.y * knockbackPwr, transform.position.z));
+        }
+        yield return 0;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //player_controller player = col.GetComponentInParent<GameObject>().GetComponent<player_controller>();
+
+        //player.Rpc_Damage(3);
+        if (col.CompareTag("Player"))
+        {
+            sound.GetComponent<AudioSource>().Play();
+            StartCoroutine(Knockback(0.02f, 30, col.transform.position));
+        }
+ 
+        
+    }
+}
---
+++ ProjectUnity/TrumpProject/Assets/Ennemi.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ennemi : MonoBehaviour {
+
+    private Vector3 pointA;
+    public Transform pointB;
+    private float speed = 1;
+
+	// Use this for initialization
+	void Start () {
+
+        pointA = transform.position;
+
+        while (true)
+        {
+            var i = Mathf.PingPong(Time.time * speed, 1);
+            transform.position = Vector3.Lerp(pointA, pointB.position, i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
