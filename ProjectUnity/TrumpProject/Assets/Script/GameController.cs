d47239e6384240299e72268ee5e0172283f748fa
alexis dupuy <alexis dupuy>
2017-02-08 11:09:58 +0100

    GameController

---
+++ ProjectUnity/TrumpProject/Assets/Script/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GUIText scoreText;
+    public int score;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        scoreText.text = "Score" + score;
+	}
+
+    public void AddScore(int bonusScore)
+    {
+        score += bonusScore;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score" + score;
+    }
+}
