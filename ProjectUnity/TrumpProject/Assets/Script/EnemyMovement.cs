1330c852f8f422b20f100990a9dc8410e12d00b1
Davydoe <the-darksyde@hotmail.fr>
2017-03-30 15:13:56 +0200

    ajout ennemy 2 + fix bullet

--- ProjectUnity/TrumpProject/Assets/Script/EnemyMovement.cs
+++ ProjectUnity/TrumpProject/Assets/Script/EnemyMovement.cs
@@ -5,6 +5,8 @@ public class EnemyMovement : MonoBehaviour {
     private Vector3 startMarker;
     private Vector3 endMarker;
     public float speed = 0.3F;
+    public Vector3 posCurrent;
+    public Vector3 posPrecedente;
 
     private float startTime;
     private float journeyLength;
@@ -15,12 +17,19 @@ public class EnemyMovement : MonoBehaviour {
         endMarker = new Vector3(3, 1, 0);
         startTime = Time.time;
         journeyLength = Vector3.Distance(startMarker, endMarker);
+      
+
     }
     void Update()
     {
         float distCovered = (Time.time - startTime) * speed;
         float fracJourney = distCovered / journeyLength;
         transform.position = Vector3.Lerp(startMarker, endMarker, fracJourney);
+       if(gameObject.transform.position.x > 0)
+        {
+            gameObject.transform.rotation = new Quaternion(-0, -180, 0, 0);
+        }
     }
+    
 }
 
--- ProjectUnity/TrumpProject/Assets/Script/PlayerMovement.cs
+++ ProjectUnity/TrumpProject/Assets/Script/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour {
     private Transform _transform;
     private Rigidbody2D _rigidbody;
     private Direction playerDirection = Direction.RIGHT;
+    private Animator m_Anim;
+    private LayerMask m_WhatIsGround;
 
     public Direction PlayerDirection
     {
@@ -18,8 +20,15 @@ public class PlayerMovement : MonoBehaviour {
         }
     }
 
-	// Use this for initialization
-	void Start () {
+    private void Awake()
+    {
+        m_Anim = GetComponent<Animator>();
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
         _transform = GetComponent(typeof (Transform)) as Transform;
         _rigidbody = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
     }
@@ -28,7 +37,13 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
         MovePlayer();
         Jump();
-
+        m_Anim.SetFloat("vSpeed", _rigidbody.velocity.y);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_transform.position, 0.2f, m_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+                isOnGround = true;
+        }
     }
 
     void MovePlayer()
@@ -36,13 +51,22 @@ public class PlayerMovement : MonoBehaviour {
         float translate = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         transform.Translate(translate, 0, 0);
 
-        if(translate > 0)
+        m_Anim.SetFloat("Speed", Mathf.Abs(speed));
+
+        if (translate > 0 && playerDirection == Direction.LEFT)
         {
             playerDirection = Direction.RIGHT;
+            Flip();
 
-        }else if(translate < 0)
+        }
+        else if(translate < 0 && playerDirection == Direction.RIGHT)
             {
             playerDirection = Direction.LEFT;
+            Flip();
+        }
+        else
+        {
+
         }
     }
 
@@ -57,10 +81,20 @@ public class PlayerMovement : MonoBehaviour {
     void OnCollisionEnter2D()
     {
         isOnGround = true;
+        m_Anim.SetBool("Ground", isOnGround);
     }
 
     void OnCollisionExit2D()
     {
         isOnGround = false;
+    }
+
+    private void Flip()
+    {
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
